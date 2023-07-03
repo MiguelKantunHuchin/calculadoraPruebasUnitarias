4baf66df0dff00f2f69d9add7904d54d969e81b2
Miguel Kantun <miguelkh21@gmail.com>
2023-07-02 22:55:37 -0600

    se crea un ejercicio de caluladora y sus pruebas unitarias

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package calculadora;
+
+public class Calculadora {
+	
+	public static int sumar(int num1, int num2){
+		
+		return num1 + num2;
+		
+	}
+	
+	
+	public static int multiplicar(int num1, int num2){
+		
+		return num1 * num2; //ponemos mal esto para buscar el error
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
